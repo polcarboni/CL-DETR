b76fac441a5e9249751005e5f45f952d1a3828f7
pol_carboni <paolo_carboni@outlook.it>
2025-02-10 10:34:04 +0100

    some runs

--- sbatch.sh
+++ sbatch.sh
@@ -8,7 +8,7 @@
 #SBATCH --error="./err/CL-DETR_%A_%a.err"
 #SBATCH --time=24:00:00
 #SBATCH --mem=40G
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --constraint="gpu_A40_48G|gpu_RTX6000_24G|gpu_RTXA5000_24G|gpu_RTX5000_16G"
 
@@ -33,15 +33,15 @@ export MASTER_PORT
 # wandb offline
 #export WANDB__SERVICE_WAIT=300
 #export PYTHONPATH=/work/tesi_pcarboni/cl_detr
-cd /work/tesi_pcarboni/cl_detr
+cd /work/tesi_pcarboni/CL-DETR
 cd ./models/ops
 sh ./make.sh
 
 arguments=(
-    "--nproc_per_node 4 /work/tesi_pcarboni/CL-DETR/main.py"
+    "/work/tesi_pcarboni/CL-DETR/main.py"
     #"/work/tesi_pcarboni/cl_detr/main.py --batch_size 4 --epochs 5 --lr 1e-2 --num_queries 50 --enc_layers 4 --dec_layers 4 --output_dir /work/tesi_pcarboni/results/exp2"
     #"/work/tesi_pcarboni/cl_detr/main.py --batch_size 4 --epochs 5 --lr 1e-3 --num_queries 50  --enc_layers 4 --dec_layers 4 --output_dir /work/tesi_pcarboni/results/exp3"
 )
 
-
+cd /work/tesi_pcarboni/CL-DETR
 sleep $(($RANDOM % 20)); srun python -u tools/launch.py ${arguments[$SLURM_ARRAY_TASK_ID]}
